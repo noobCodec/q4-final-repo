46fce1216003873af943135d616de33fd1f3a0dc
noobCodec <peter.flis@donboscoprep.org>
2021-10-25 23:47:16 -0400

    xdsa

--- game/ai/plant_test.cpp
+++ game/ai/plant_test.cpp
@@ -1,5 +1,4 @@
 
-
 #include "../../idlib/precompiled.h"
 #pragma hdrstop
 
@@ -12,11 +11,10 @@ public:
 
 	plant_test(void);
 
-	void				InitSpawnArgsVariables(void);
 	void				Spawn(void);
 	void				Save(idSaveGame* savefile) const;
 	void				Restore(idRestoreGame* savefile);
-
+	
 	virtual bool		Pain(idEntity* inflictor, idEntity* attacker, int damage, const idVec3& dir, int location);
 
 protected:
@@ -26,17 +24,9 @@ protected:
 	stateResult_t		State_Combat(const stateParms_t& parms);
 	stateResult_t		State_Killed(const stateParms_t& parms);
 
-	int					shieldHealth;
-	int					maxShots;
-	int					minShots;
-	int					shots;
-
 private:
-
-	rvAIAction			actionBlasterAttack;
-
-	stateResult_t		State_Torso_BlasterAttack(const stateParms_t& parms);
-
+	idEntity* seeds[10]		;
+	char i = 'a';
 	CLASS_STATES_PROTOTYPE(plant_test);
 };
 
@@ -49,26 +39,14 @@ rvMonsterTurret::rvMonsterTurret
 ================
 */
 plant_test::plant_test() {
-	shieldHealth = 0;
 }
 
-void plant_test::InitSpawnArgsVariables(void) {
-	maxShots = spawnArgs.GetInt("maxShots", "1");
-	minShots = spawnArgs.GetInt("minShots", "1");
-}
 /*
 ================
 rvMonsterTurret::Spawn
 ================
 */
 void plant_test::Spawn(void) {
-	actionBlasterAttack.Init(spawnArgs, "action_blasterAttack", "Torso_BlasterAttack", AIACTIONF_ATTACK);
-
-	shieldHealth = spawnArgs.GetInt("shieldHealth");
-	health += shieldHealth;
-
-	InitSpawnArgsVariables();
-	shots = 0;
 }
 
 /*
@@ -77,9 +55,6 @@ rvMonsterTurret::Save
 ================
 */
 void plant_test::Save(idSaveGame* savefile) const {
-	savefile->WriteInt(shieldHealth);
-	savefile->WriteInt(shots);
-	actionBlasterAttack.Save(savefile);
 }
 
 /*
@@ -88,11 +63,6 @@ rvMonsterTurret::Restore
 ================
 */
 void plant_test::Restore(idRestoreGame* savefile) {
-	savefile->ReadInt(shieldHealth);
-	savefile->ReadInt(shots);
-	actionBlasterAttack.Restore(savefile);
-
-	InitSpawnArgsVariables();
 }
 
 /*
@@ -102,9 +72,6 @@ rvMonsterTurret::CheckActions
 */
 bool plant_test::CheckActions(void) {
 	// Attacks
-	if (PerformAction(&actionBlasterAttack, (checkAction_t)&idAI::CheckAction_RangedAttack, &actionTimerRangedAttack)) {
-		return true;
-	}
 	return idAI::CheckActions();
 }
 
@@ -114,17 +81,21 @@ rvMonsterTurret::Pain
 ================
 */
 bool plant_test::Pain(idEntity* inflictor, idEntity* attacker, int damage, const idVec3& dir, int location) {
-	// Handle the shield effects
-	if (shieldHealth > 0) {
-		shieldHealth -= damage;
-		if (shieldHealth <= 0) {
-			PlayEffect("fx_shieldBreak", GetPhysics()->GetOrigin(), (-GetPhysics()->GetGravityNormal()).ToMat3());
-		}
-		else {
-			PlayEffect("fx_shieldHit", GetPhysics()->GetOrigin(), (-GetPhysics()->GetGravityNormal()).ToMat3());
-		}
-	}
-
+	common->Printf("i am in pain");
+	idDict	  args;
+	idEntity* newLegs;
+	//We may need to do this at a later point
+	//SetSkin ( declManager->FindSkin	 ( spawnArgs.GetString ( "skin_legs" ) ) );
+		args.Copy(*gameLocal.FindEntityDefDict("env_ragdoll_marine"));
+		idVec3 test = GetPhysics()->GetOrigin();
+		test.z +=i+2;
+		args.SetVector("origin", test);
+		args.SetInt("angle", move.current_yaw);
+		gameLocal.SpawnEntityDef(args, &newLegs);
+		//store the name of the entity in the Makron's keys so we can burn it out as well.
+		common->Printf(newLegs->GetName());
+		newLegs->Bind(this, 0);
+		i+=3;
 	return idAI::Pain(inflictor, attacker, damage, dir, location);
 }
 
@@ -139,8 +110,6 @@ bool plant_test::Pain(idEntity* inflictor, idEntity* attacker, int damage, const
 CLASS_STATES_DECLARATION(plant_test)
 STATE("State_Combat", plant_test::State_Combat)
 STATE("State_Killed", plant_test::State_Killed)
-
-STATE("Torso_BlasterAttack", plant_test::State_Torso_BlasterAttack)
 END_CLASS_STATES
 
 /*
@@ -169,53 +138,7 @@ rvMonsterTurret::State_Killed
 */
 stateResult_t plant_test::State_Killed(const stateParms_t& parms) {
 	gameLocal.PlayEffect(gameLocal.GetEffect(spawnArgs, "fx_death"), GetPhysics()->GetOrigin(), (-GetPhysics()->GetGravityNormal()).ToMat3());
+	
 	return idAI::State_Killed(parms);
 }
 
-/*
-================
-rvMonsterTurret::State_Torso_BlasterAttack
-================
-*/
-stateResult_t plant_test::State_Torso_BlasterAttack(const stateParms_t& parms) {
-	enum {
-		STAGE_INIT,
-		STAGE_FIRE,
-		STAGE_WAIT,
-	};
-	switch (parms.stage) {
-	case STAGE_INIT:
-		DisableAnimState(ANIMCHANNEL_LEGS);
-		shots = (minShots + gameLocal.random.RandomInt(maxShots - minShots + 1)) * combat.aggressiveScale;
-		return SRESULT_STAGE(STAGE_FIRE);
-
-	case STAGE_FIRE:
-		PlayAnim(ANIMCHANNEL_TORSO, "range_attack", 2);
-		return SRESULT_STAGE(STAGE_WAIT);
-
-	case STAGE_WAIT:
-		if (AnimDone(ANIMCHANNEL_TORSO, 2)) {
-			if (--shots <= 0) {
-				return SRESULT_DONE;
-			}
-			return SRESULT_STAGE(STAGE_FIRE);
-		}
-		return SRESULT_WAIT;
-	}
-	return SRESULT_ERROR;
-}
-
-/*//spawn in a new Makron leg,			
-	idDict	  args;
-	idEntity*  newLegs;
-
-	//We may need to do this at a later point
-	//SetSkin ( declManager->FindSkin	 ( spawnArgs.GetString ( "skin_legs" ) ) );
-
-	args.Copy ( *gameLocal.FindEntityDefDict ( "monster_makron_legs" ) );
-	args.SetVector ( "origin", GetPhysics()->GetOrigin() );
-	args.SetInt ( "angle", move.current_yaw );
-	gameLocal.SpawnEntityDef ( args, &newLegs );
-
-	//store the name of the entity in the Makron's keys so we can burn it out as well.
-	spawnArgs.Set( "legs_name", newLegs->GetName() );*/
